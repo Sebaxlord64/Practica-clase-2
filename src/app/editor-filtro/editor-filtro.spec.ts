f7f2af431f3c621625653e452b62475e55e491b9
Sebaxlord64 <mauriciovasgui924@gmail.com>
2026-02-18 20:50:17 -0400

    Primer commit prac2

---
+++ src/app/editor-filtro/editor-filtro.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditorFiltroComponent } from './editor-filtro';
+
+describe('EditorFiltro', () => {
+  let component: EditorFiltroComponent;
+  let fixture: ComponentFixture<EditorFiltroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EditorFiltroComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EditorFiltroComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/editor-filtro/editor-filtro.ts
@@ -0,0 +1,27 @@
+import { Component, signal, computed } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-filtro',
+  standalone: true,
+  templateUrl: './editor-filtro.html',
+  styleUrl: './editor-filtro.scss',
+})
+export class EditorFiltroComponent {
+
+  brillo = signal(100);
+  contraste = signal(100);
+  blur = signal(0);
+
+  filtroCss = computed(() => {
+    return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px)`;
+  });
+
+  actualizar(prop: string, evento: Event) {
+    const valor = +(evento.target as HTMLInputElement).value;
+
+    if (prop === 'brillo') this.brillo.set(valor);
+    if (prop === 'contraste') this.contraste.set(valor);
+    if (prop === 'blur') this.blur.set(valor);
+  }
+
+}
